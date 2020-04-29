66be65ba40d8dcb4b2b59503d4532b6fd8e5593a
Onur Alp Baklaci <onuralpb@gmail.com>
2020-04-29 23:43:57 +0300

    xcxz

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./assets/css/main.scss";
-import "./App.scss";
+// import "./App.scss";
 import Async from "react-code-splitting";
 import { connect } from "react-redux";
 import { updateUser } from "./actions/userActions";
@@ -17,82 +17,138 @@ import Comments from "./components/rightPanel/comments";
 import Photos from "./components/rightPanel/photos";
 import Todos from "./components/rightPanel/todos";
 
-const Users = () => <Async load={import(/* webpackChunkName: "Users" */ "./components/rightPanel/users")} />;
-const Jquery = () => <Async load={import(/* webpackChunkName: "jquery" */ "./components/rightPanel/jquery")} />;
-const Products = () => <Async load={import(/* webpackChunkName: "products" */ "./components/rightPanel/products")} />;
+const Users = () => (
+  <Async
+    load={import(
+      /* webpackChunkName: "Users" */ "./components/rightPanel/users"
+    )}
+  />
+);
+const Jquery = () => (
+  <Async
+    load={import(
+      /* webpackChunkName: "jquery" */ "./components/rightPanel/jquery"
+    )}
+  />
+);
+const Products = () => (
+  <Async
+    load={import(
+      /* webpackChunkName: "products" */ "./components/rightPanel/products"
+    )}
+  />
+);
 
 class App extends Component {
-	state = {
-		posts          : [],
-		isLoading      : true,
-		animationName  : "flipInX",
-		animationSpeed : "faster",
-	};
+  state = {
+    posts: [],
+    isLoading: true,
+    animationName: "flipInX",
+    animationSpeed: "faster",
+  };
 
-	dataFromPost = (appData) => {
-		this.setState({
-			posts     : appData,
-			isLoading : false,
-		});
-	};
+  dataFromPost = appData => {
+    this.setState({
+      posts: appData,
+      isLoading: false,
+    });
+  };
 
-	_updateUser = (e) => {
-		this.props._updateUser("Mete");
-		addAnimate(e.target, this.state.animationName, this.state.animationSpeed);
-	};
+  _updateUser = e => {
+    this.props._updateUser("Mete");
+    addAnimate(e.target, this.state.animationName, this.state.animationSpeed);
+  };
 
-	render() {
-		console.log("App.js > this.props in render : ", this);
-		const { fetching } = this.props.albums;
-		return (
-			<Router>
-				<div className='row align-items-stretch'>
-					<div className='col-2 leftPanel'>
-						<Logo />
-						<hr />
-						<NavMenu {...this.props} />
-						<h2 className='text-center'>{this.props.user}</h2>
-						<button
-							className={`btn ${fetching ? "btn-success " : "btn-danger "} btn-block`}
-							type='button'
-							onClick={this._updateUser}
-						>
-							{fetching ? "Loading..." : "Warning"}
-						</button>
-					</div>
-					<div className='col-10 rightPanel'>
-						<Header {...this.props} />
+  render() {
+    console.log("App.js > this.props in render : ", this);
+    const { fetching } = this.props.albums;
+    return (
+      <Router>
+        <div className="row align-items-stretch">
+          <div className="col-2 leftPanel">
+            <Logo />
+            <hr />
+            <NavMenu {...this.props} />
+            <h2 className="text-center">{this.props.user}</h2>
+            <button
+              className={`btn ${
+                fetching ? "btn-success " : "btn-danger "
+              } btn-block`}
+              type="button"
+              onClick={this._updateUser}
+            >
+              {fetching ? "Loading..." : "Warning"}
+            </button>
+          </div>
+          <div className="col-10 rightPanel">
+            <Header {...this.props} />
 
-						<div className='listContainer'>
-							<Route path='/' exact render={() => <Dashboard {...this.props} />} />
-							<Route path='/posts' exact render={() => <Posts {...this.state} dataFromPost={this.dataFromPost} />} />
-							<Route
-								path='/comments'
-								exact
-								render={() => <Comments comments={this.state.comments} isLoading={this.state.isLoading} />}
-							/>
-							<Route path='/albums' exact render={() => <Albums {...this.props} />} />
-							<Route path='/photos' exact render={() => <Photos {...this.props} />} />
-							<Route path='/todos' exact render={() => <Todos {...this.state} />} />
-							<Route path='/users' exact component={Users} render={() => <Users {...this.props} />} />
-							<Route path='/jquery' exact component={Jquery} />
-							<Route path='/products' exact render={() => <Products {...this.props} />} />
-						</div>
-					</div>
-				</div>
-			</Router>
-		);
-	}
+            <div className="listContainer">
+              <Route
+                path="/"
+                exact
+                render={() => <Dashboard {...this.props} />}
+              />
+              <Route
+                path="/posts"
+                exact
+                render={() => (
+                  <Posts {...this.state} dataFromPost={this.dataFromPost} />
+                )}
+              />
+              <Route
+                path="/comments"
+                exact
+                render={() => (
+                  <Comments
+                    comments={this.state.comments}
+                    isLoading={this.state.isLoading}
+                  />
+                )}
+              />
+              <Route
+                path="/albums"
+                exact
+                render={() => <Albums {...this.props} />}
+              />
+              <Route
+                path="/photos"
+                exact
+                render={() => <Photos {...this.props} />}
+              />
+              <Route
+                path="/todos"
+                exact
+                render={() => <Todos {...this.state} />}
+              />
+              <Route
+                path="/users"
+                exact
+                component={Users}
+                render={() => <Users {...this.props} />}
+              />
+              <Route path="/jquery" exact component={Jquery} />
+              <Route
+                path="/products"
+                exact
+                render={() => <Products {...this.props} />}
+              />
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		...state,
-	};
+const mapStateToProps = state => {
+  return {
+    ...state,
+  };
 };
 
 const mapDispatchToProps = {
-	_updateUser : updateUser,
+  _updateUser: updateUser,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -2,92 +2,93 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
-	mode         : "development",
-	entry        : [
-		"react-hot-loader/patch",
-		"webpack/hot/only-dev-server",
-		"webpack-dev-server/client?http://localhost:3004/",
-		"./src/index.js",
-	],
-	cache        : false,
-	//devtool: "source-map",  add .map files
-	output       : {
-		filename      : "[name].js",
-		chunkFilename : "[name].js",
-		path          : path.resolve(__dirname, "dist"),
-	},
-	devServer    : {
-		contentBase        : path.join(__dirname, "src", "public"),
-		port               : 3004,
-		hot                : true,
-		inline             : true,
-		open               : true,
-		historyApiFallback : true,
-	},
-	optimization : {
-		minimize    : true,
-		splitChunks : {
-			chunks : "all",
-		},
-	},
-	module       : {
-		rules : [
-			{
-				test    : /\.(js|jsx)$/,
-				exclude : /node_modules/,
-				loader  : "babel-loader",
-				options : {
-					presets : [ "@babel/env", "@babel/react" ],
-				},
-			},
-			{
-				test    : /\.(css|scss)$/,
-				loaders : [
-					{
-						loader : "style-loader",
-					},
-					{
-						loader : "css-loader",
-					},
-					{
-						loader : "sass-loader",
-					},
-				],
-			},
-			{
-				test : /\.html$/,
-				use  : [
-					{
-						loader : "html-loader",
-					},
-				],
-			},
-			{
-				test    : /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-				loaders : "file-loader",
-				options : {
-					name             : "[path][name].[ext]",
-					context          : path.resolve(__dirname, "src/assets/img/"),
-					outputPath       : "img/",
-					publicPath       : "/",
-					useRelativePaths : true,
-				},
-			},
-		],
-	},
-	resolve      : {
-		modules : [ path.resolve(__dirname, "src"), "node_modules" ],
-	},
-	plugins      : [
-		new webpack.DefinePlugin({ "process.env.NODE_ENV": '"production"' }),
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebPackPlugin({
-			template : path.join(__dirname, "public", "index.html"),
-		}),
-		//new CompressionPlugin()
-		// new BundleAnalyzerPlugin(),
-	],
+  mode: "development",
+  entry: [
+    "react-hot-loader/patch",
+    "webpack/hot/only-dev-server",
+    "webpack-dev-server/client?http://localhost:3004/",
+    "./src/index.js",
+  ],
+  cache: false,
+  //devtool: "source-map",  add .map files
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "src", "public"),
+    port: 3004,
+    hot: true,
+    inline: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env", "@babel/react"],
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|otf|woff|jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+          context: path.resolve(__dirname, "src/assets/img/"),
+          outputPath: "img/",
+          publicPath: "/",
+          useRelativePaths: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  plugins: [
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": '"production"' }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+    //new CompressionPlugin()
+    // new BundleAnalyzerPlugin(),
+  ],
 };
